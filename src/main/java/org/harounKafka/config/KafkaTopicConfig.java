0280cf648ec70274f46d4aba785a68fa68efe8fb
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-12-27 23:27:32 +0100

    KafkaTempalte + Producer Config

---
+++ src/main/java/org/harounKafka/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package org.harounKafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic harounTopic() {
+        return TopicBuilder.name("haroun-topic")
+                .build();
+    }
+}
